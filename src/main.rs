e1311a34c2e1d63dd83f8ec8fda0f10cf06d0cbf
Vortex Acherontic <vortex@z-ray.de>
2023-11-04 19:42:27 +0100

    Hello vulkan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use bytemuck::{Pod, Zeroable};
+
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer, TypedBufferAccess};
 use vulkano::command_buffer::allocator::StandardCommandBufferAllocator;
 use vulkano::command_buffer::{
     AutoCommandBufferBuilder, CommandBufferUsage, RenderPassBeginInfo, SubpassContents,
@@ -7,8 +10,12 @@ use vulkano::device::{Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInf
 use vulkano::image::view::ImageView;
 use vulkano::image::{ImageAccess, SwapchainImage};
 use vulkano::instance::{Instance, InstanceCreateInfo};
-use vulkano::pipeline::graphics::viewport::Viewport;
-use vulkano::render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass};
+use vulkano::memory::allocator::StandardMemoryAllocator;
+use vulkano::pipeline::graphics::input_assembly::InputAssemblyState;
+use vulkano::pipeline::graphics::vertex_input::BuffersDefinition;
+use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
+use vulkano::pipeline::GraphicsPipeline;
+use vulkano::render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass, Subpass};
 use vulkano::swapchain::{
     self, AcquireError, Swapchain, SwapchainCreateInfo, SwapchainCreationError,
     SwapchainPresentInfo,
@@ -20,9 +27,17 @@ use vulkano_win::VkSurfaceBuild;
 
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::{Window, WindowBuilder, Fullscreen};
+use winit::window::{Window, WindowBuilder};
 
 use std::sync::Arc;
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default, Zeroable, Pod)]
+struct Vertex {
+    position: [f32; 3], //Use the same name in the vertex shader
+    color: [f32; 3], //Use the same name in the vertex shader
+}
+vulkano::impl_vertex!(Vertex, position, color);
 
 fn main() {
     let instance = {
@@ -142,8 +157,44 @@ fn main() {
 
     let command_buffer_allocator =
         StandardCommandBufferAllocator::new(device.clone(), Default::default());
+    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 
-    // shaders would go here
+    mod vs {
+        vulkano_shaders::shader! {
+            ty: "vertex",
+            src: "
+                #version 450
+                layout(location = 0) in vec3 position;
+                layout(location = 1) in vec3 color;
+
+                layout(location = 0) out vec3 out_color;
+    
+                void main() {
+                    gl_Position = vec4(position, 1.0);
+                    out_color = color;
+                }
+            "
+        }
+    }
+
+    mod fs {
+        vulkano_shaders::shader! {
+            ty: "fragment",
+            src: "
+                #version 450
+                layout(location = 0) in vec3 in_color;
+
+                layout(location = 0) out vec4 f_color;
+    
+                void main() {
+                    f_color = vec4(in_color, 1.0);
+                }
+            "
+        }
+    }
+
+    let vs = vs::load(device.clone()).unwrap();
+    let fs = fs::load(device.clone()).unwrap();
 
     let render_pass = vulkano::single_pass_renderpass!(
         device.clone(),
@@ -162,7 +213,56 @@ fn main() {
     )
     .unwrap();
 
-    // pipeline would be declared here
+    let pipeline = GraphicsPipeline::start()
+        .vertex_input_state(BuffersDefinition::new().vertex::<Vertex>())
+        .vertex_shader(vs.entry_point("main").unwrap(), ())
+        .input_assembly_state(InputAssemblyState::new())
+        .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
+        .fragment_shader(fs.entry_point("main").unwrap(), ())
+        .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+        .build(device.clone())
+        .unwrap();
+
+    //Mesh data
+    let vertices = [
+        Vertex {
+            position: [-0.5, 0.5, 0.0],
+            color: [1.0, 0.0, 0.0],
+        },
+        Vertex {
+            position: [0.5, 0.5, 0.0],
+            color: [0.0, 1.0, 0.0],
+        },
+        Vertex {
+            position: [0.0, -0.5, 0.0],
+            color: [0.0, 0.0, 1.0],
+        },
+
+        Vertex {
+            position: [0.5, 1.5, 0.0],
+            color: [0.0, 1.0, 0.0],
+        },
+        Vertex {
+            position: [1.5, 1.5, 0.0],
+            color: [0.0, 1.0, 0.0],
+        },
+        Vertex {
+            position: [1.0, 0.5, 0.0],
+            color: [0.0, 0.0, 1.0],
+        },
+    ];
+
+    //Buffer with mesh data passed to the GPU to render
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(
+        &memory_allocator,
+        BufferUsage {
+            vertex_buffer: true,
+            ..BufferUsage::empty()
+        },
+        false,
+        vertices,
+    )
+    .unwrap();
 
     let mut viewport = Viewport {
         origin: [0.0, 0.0],
@@ -248,6 +348,11 @@ fn main() {
                     },
                     SubpassContents::Inline,
                 )
+                .unwrap()
+                .set_viewport(0, [viewport.clone()])
+                .bind_pipeline_graphics(pipeline.clone())
+                .bind_vertex_buffers(0, vertex_buffer.clone())
+                .draw(vertex_buffer.len() as u32, 1, 0, 0)
                 .unwrap()
                 .end_render_pass()
                 .unwrap();
